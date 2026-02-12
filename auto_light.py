d95b73da11ae3727f3a1035c5df2ce47de233093
Fhntv <artemav.007@gmail.com>
2026-02-12 20:01:14 +0300

    все

---
+++ auto_light.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 26  
+sensor = 6 
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(sensor, GPIO.IN)  
+
+while True:
+  
+    GPIO.output(led, not GPIO.input(sensor))
